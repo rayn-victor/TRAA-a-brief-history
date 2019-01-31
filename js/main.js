413a071763a7819bb4e0784d7a6820b3d32447f0
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2019-01-31 11:11:51 -0500

    added scripts and design

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//create jar, name jar, place string in jar
+//var firstName = "Rayn";
+
+ //alert method creats an annoying popup, plus adds two strings together
+//alert("Hello, " + firstName);
+
+//modern js version of that
+//alert(`Hello,  ${firstName}`)
+
+console.log("javascript linked up");
